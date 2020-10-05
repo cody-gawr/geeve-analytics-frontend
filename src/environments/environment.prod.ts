1b5e99fd7aef29d79178d2b30a7760f5b2f43409
Shivika Sharma <sshivika@teqmavens.com>
2020-10-05 22:32:33 +0530

    updates

--- src/environments/environment.prod.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-  production: true,
- solutionsUrl : "https://api.jeeve.com.au/staging/solutions",
-apiUrl :"https://api.jeeve.com.au/staging/members",
-  homeUrl:"https://staging-members.jeeve.com.au",
-};
