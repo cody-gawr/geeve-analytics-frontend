6161c3f3a01fa1477e463e31e020e48733b20b6d
GT <gary@jeeve.com.au>
2020-12-14 08:17:52 +0000

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,8 +1,6 @@
 export const environment = {
   production: true,
- // apiUrl : "http://localhost/jeeveanalytics/server/",
- //  homeUrl:"http://localhost/jeeveanalytics/client2/"
- apiUrl : "https://api.jeeve.com.au/analytics",
+  apiUrl : "https://api.jeeve.com.au/analytics",
   homeUrl:"https://analytics.jeeve.com.au/",
   solutionsUrl : "https://api.jeeve.com.au/solutions"
 };
