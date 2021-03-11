ebe82cf8228c3a7e42c86204dd631638ddff4005
Hanney Sharma <shanney@teqmavens.com>
2021-03-11 15:07:49 +0530

    update

--- src/app/dashboards/healthscreen/healthscreen.service.ts
+++ src/app/dashboards/healthscreen/healthscreen.service.ts
@@ -30,7 +30,7 @@ export class HealthScreenService {
     // Dentist Production Service
     healthCheckStats( clinic_id, user_id = this._cookieService.get("userid") ,token = this._cookieService.get("token") ): Observable<any> {
         var header = this.getHeaders(); 
-        return this.http.get(this.apiUrl +"/HealthChecks/healthCheckStats?user_id="+user_id+"&clinic_id="+clinic_id, { headers: header })
+        return this.http.get(this.apiUrl +"/health/healthCheckStats?user_id="+user_id+"&clinic_id="+clinic_id, { headers: header })
         .pipe(map((response: Response) => {
                         return response;
                     })
