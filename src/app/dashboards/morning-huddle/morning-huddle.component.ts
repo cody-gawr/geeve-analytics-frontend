a3bc7873248404a60054cb943f5291a8227ed0fd
Hanney Sharma <shanney@teqmavens.com>
2021-04-22 17:28:14 +0530

    Finances Trend mode issue when xero not connected

--- src/app/dashboards/morning-huddle/morning-huddle.component.ts
+++ src/app/dashboards/morning-huddle/morning-huddle.component.ts
@@ -344,7 +344,7 @@ initiate_clinic() {
    getChairUtilisationRate(){
     this.morningHuddleService.getChairUtilisationRate( this.clinic_id, this.previousDays,  this.user_type  ).subscribe((production:any) => {
       if(production.message == 'success') {
-        this.todayChairUtilisationRate = production.data;
+        this.todayChairUtilisationRate =  Math.round(production.data);
 
     
       }
