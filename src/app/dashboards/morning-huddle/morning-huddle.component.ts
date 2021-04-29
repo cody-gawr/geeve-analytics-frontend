5d5ca515e878debbd789daa19cbd22dabbc5bd74
Hanney Sharma <shanney@teqmavens.com>
2021-04-29 18:23:31 +0530

    Mh dentist checks update

--- src/app/dashboards/morning-huddle/morning-huddle.component.ts
+++ src/app/dashboards/morning-huddle/morning-huddle.component.ts
@@ -45,6 +45,7 @@ export class MorningHuddleComponent implements OnInit,OnDestroy {
 	 public id:any = '';
   	public clinic_id:any = '';
   	public user_type:any = '';
+    public dentistid:any = null;
     public production:any = '';
     public recallRate:any = '';
     public treatmentRate:any = '';
@@ -109,6 +110,11 @@ export class MorningHuddleComponent implements OnInit,OnDestroy {
  ngOnInit(){
     $('#currentDentist').attr('did','all');
     this.user_type = this._cookieService.get("user_type");
+    if(this._cookieService.get("dentistid") && this._cookieService.get("dentistid") != '' && this.user_type == '4'){
+        this.dentistid = this._cookieService.get("dentistid");
+    }
+
+    this.user_type = this._cookieService.get("user_type");
     
      //this.initiate_clinic();
  }
@@ -123,15 +129,13 @@ ngOnDestroy() {
 }
 
 initiate_clinic() {
-    $('.external_clinic').show();
-    $('.dentist_dropdown').hide();
-    $('.header_filters').addClass('flex_direct_mar');
-    $('.header_filters').removeClass('hide_header');
-
-    var val = $('#currentClinic').attr('cid');
-   if(val != undefined && val !='all') {
-      this.clinic_id = val;
-      
+  $('.external_clinic').show();
+  $('.dentist_dropdown').hide();
+  $('.header_filters').addClass('flex_direct_mar');
+  $('.header_filters').removeClass('hide_header');
+  var val = $('#currentClinic').attr('cid');
+  if(val != undefined && val !='all') {
+    this.clinic_id = val;
     $('#title').html('Morning Huddle');
     if(this.previousDays  == '')
     {
@@ -143,35 +147,32 @@ initiate_clinic() {
     this.getTreatmentRate();
     this.getDentistList();
     /***** Tab 1 ***/    
+    /***** Tab 2 ***/
     //this.getSchedulePatients(null);
     this.getScheduleNewPatients(null);
     this.getScheduleHours(null);
     this.getUnscheduleHours(null);
     this.getAppointmentCards(null);
     /***** Tab 2 ***/
-    
-    /***** Tab 2 ***/
 
-    /***** Tab 3 ***/
-
-    // this.getReAppointment();
-    //this.getUnscheduledPatients();
-    this.getUnscheduledValues();
-    //this.getTodayPatients();
-    this.getTodayUnscheduledHours();
-    this.getChairUtilisationRate();
-    this.getTodayUnscheduledBal();
-    this.getNoShow();
-    //this.getTodayPostopCalls();
-    /***** Tab 3 ***/
-    
-    /***** Tab 4 ***/
-    this.getReminders();
-    /***** Tab 4 ***/
-    /***** Tab 5 ***/
-    this.getFollowupsUnscheduledPatients();
-    this.getFollowupPostOpCalls();
-    /***** Tab 5 ***/
+    if(this.user_type != '4'){
+      /***** Tab 3 ***/
+      this.getUnscheduledValues();
+      this.getTodayUnscheduledHours();
+      this.getChairUtilisationRate();
+      this.getTodayUnscheduledBal();
+      this.getNoShow();
+      //this.getUnscheduledPatients();
+      //this.getTodayPatients();
+      //this.getTodayPostopCalls();
+      // this.getReAppointment();
+      /***** Tab 3 ***/ 
+      /***** Tab 4 ***/
+      this.getReminders();
+      this.getFollowupsUnscheduledPatients();
+      this.getFollowupPostOpCalls();
+      /***** Tab 4 ***/     
+      }
     }
   }
 
@@ -425,6 +426,9 @@ initiate_clinic() {
         this.clinicDentists = [];
         this.appointmentCards.data = production.data; 
         this.appointmentCardsTemp = production.data; 
+        if(this.user_type == '4'){
+          this.refreshScheduleTab(this.dentistid);
+        }
         production.data.forEach(val => {
           // check for duplicate values        
           var isExsist = this.clinicDentists.filter(function (person) { return person.provider_id == val.provider_id });
