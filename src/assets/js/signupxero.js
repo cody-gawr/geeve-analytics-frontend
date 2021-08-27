bd7019e43503297579665b66bffd82394e3ad47a
Hanney Sharma <shanney@teqmavens.com>
2021-08-27 15:09:02 +0530

    mailerlite live js link remove

--- src/assets/js/signupxero.js
+++ src/assets/js/signupxero.js
@@ -2,5 +2,5 @@
 var c={ a:arguments,q:[]};var r=this.push(c);return "number"!=typeof r?r:f.bind(c.q);}
 f.q=f.q||[];m[e]=m[e]||f.bind(f.q);m[e].q=m[e].q||f.q;r=a.createElement(i);
 var _=a.getElementsByTagName(i)[0];r.async=1;r.src=l+'?v'+(~~(new Date().getTime()/1000000));
-_.parentNode.insertBefore(r,_);})(window, document, 'script', 'https://static.mailerlite.com/js/universal.js', 'ml');
+_.parentNode.insertBefore(r,_);})
 var ml_account = ml('accounts', '2348549', 'w4o2f2u2e9', 'load');
