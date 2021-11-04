31ececf7e43a1546a14fe2599711e41fa983173e
GT <gary@jeeve.com.au>
2021-11-04 11:24:38 +1000

    direct pilot to prod api

--- src/environments/environment.pilot.ts
+++ src/environments/environment.pilot.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: true,
  // apiUrl : "http://localhost/jeeveanalytics/server/",
  //  homeUrl:"http://localhost/jeeveanalytics/client2/"
- apiUrl : "https://pilot-api.jeeve.com.au/pilot/analytics",
+ apiUrl : "https://api.jeeve.com.au/analytics",
   homeUrl:"https://pilot-analytics.jeeve.com.au/",
-  solutionsUrl : "https://pilot-api.jeeve.com.au/pilot/solutions"
+  solutionsUrl : "https://api.jeeve.com.au/solutions"
 };
