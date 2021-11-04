4388e623c38fdb14a5e92588de1e14d644eb97d9
GT <gary@jeeve.com.au>
2021-11-04 10:29:12 +1000

    added changes for pilot env

---
+++ src/environments/environment.pilot.ts
@@ -0,0 +1,8 @@
+export const environment = {
+  production: true,
+ // apiUrl : "http://localhost/jeeveanalytics/server/",
+ //  homeUrl:"http://localhost/jeeveanalytics/client2/"
+ apiUrl : "https://pilot-api.jeeve.com.au/pilot/analytics",
+  homeUrl:"https://pilot-analytics.jeeve.com.au/",
+  solutionsUrl : "https://pilot-api.jeeve.com.au/pilot/solutions"
+};
