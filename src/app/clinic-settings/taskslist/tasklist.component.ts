9dfc13c665ad5256bb7d7fac36b78228c472e74d
Hanney Sharma <shanney@teqmavens.com>
2021-11-26 18:47:45 +0530

    Update code

--- src/app/clinic-settings/taskslist/tasklist.component.ts
+++ src/app/clinic-settings/taskslist/tasklist.component.ts
@@ -111,8 +111,7 @@ export class DialogOverviewTasklistDialogComponent {
     // this.dialogRef.componentInstance.data.tasksListItems[i].readOnly = !this.dialogRef.componentInstance.data.tasksListItems[i].readOnly
   }
   removeItem(i) {
-<<<<<<< HEAD
-    let data = this.dialogRef.componentInstance.data.tasksListItems[i];
+  let data = this.dialogRef.componentInstance.data.tasksListItems[i];
     if (data) {
       this.taskService.deleteTasksItem(data.id, data.clinic_id).subscribe((res) => {
         if (res.message == 'success') {
@@ -124,9 +123,6 @@ export class DialogOverviewTasklistDialogComponent {
         console.log('error', error)
       });
     }
-=======
-    this.dialogRef.componentInstance.data.tasksListItems.splice(i, 1);
->>>>>>> bfed29ac760d2609f7dfd186da0de5152ad5c0f2
   }
 
   additem() {
