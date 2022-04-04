f96a55a2ec45bc6972cbc80e511b1c1d4481dbeb
GT <gary@jeeve.com.au>
2022-04-05 07:52:08 +1000

    fix pilot env file

--- src/environments/environment.pilot.ts
+++ src/environments/environment.pilot.ts
@@ -1,8 +1,6 @@
 export const environment = {
   production: true,
- // apiUrl : "http://localhost/jeeveanalytics/server/",
- //  homeUrl:"http://localhost/jeeveanalytics/client2/"
- apiUrl : "https://api.jeeve.com.au/analytics",
+ apiUrl : "https://pilot-api.jeeve.com.au/pilot/analytics",
   homeUrl:"https://pilot-analytics.jeeve.com.au/",
-  solutionsUrl : "https://api.jeeve.com.au/solutions"
+  solutionsUrl : "https://pilot-api.jeeve.com.au/pilot/solutions"
 };
