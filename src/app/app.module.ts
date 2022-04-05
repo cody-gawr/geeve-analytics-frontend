4144f1d21e9a225a631cb4d65325ec4570c647cf
HanneySharma <shanney@teqmavens.com>
2022-04-05 15:16:55 +0530

    Implement print

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -46,6 +46,7 @@ import { DashboardDurationTabsComponent } from './dashboards/dashboard-duration-
 import { NgxDaterangepickerMd } from 'ngx-daterangepicker-material';
 import { KanbanAllModule,KanbanModule, KanbanComponent,KanbanModel  } from '@syncfusion/ej2-angular-kanban';
 import { DropDownListAllModule } from '@syncfusion/ej2-angular-dropdowns';
+import {NgxPrintModule} from 'ngx-print';
 const DEFAULT_PERFECT_SCROLLBAR_CONFIG: PerfectScrollbarConfigInterface = {
   suppressScrollX: true,
   wheelSpeed: 2,
@@ -87,6 +88,7 @@ const DEFAULT_PERFECT_SCROLLBAR_CONFIG: PerfectScrollbarConfigInterface = {
   imports: [
     UserIdleModule.forRoot({idle: 14400, timeout: 10, ping: 10}),
     MatMenuModule,
+    NgxPrintModule,
     DemoMaterialModule,
     BrowserModule,
     BrowserAnimationsModule,
