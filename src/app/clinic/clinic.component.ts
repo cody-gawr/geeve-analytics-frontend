869abff15e9348be7c0ed570ea750e3f4eca21db
Hanney Sharma <shanney@teqmavens.com>
2022-12-09 12:51:25 +0530

    added exact option to add clinic

--- src/app/clinic/clinic.component.ts
+++ src/app/clinic/clinic.component.ts
@@ -31,7 +31,7 @@ export class DialogOverviewExampleDialogComponent {
   public isConnectedCore :boolean = false;
   public showConnectButton : boolean = false;
   public urlPattern=/^(?:(?:https?|ftp):\/\/)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:\/\S*)?$/;
-
+  public pmsValue = "";
   constructor(private fb: FormBuilder,
     public dialogRef: MatDialogRef<DialogOverviewExampleDialogComponent>,
     @Inject(MAT_DIALOG_DATA) public data: any
@@ -72,6 +72,7 @@ export class DialogOverviewExampleDialogComponent {
     this.isConnectedCore = true;
   }
   selectPMS(data){
+    this.pmsValue = data;
     this.showConnectButton = (data == 'core') ? true : false;
     if(this.showConnectButton){
       this.form.get('coreURL').setValidators([Validators.required, Validators.pattern(this.urlPattern)]);
