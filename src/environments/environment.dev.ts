d5131e94724c7c4e23286ea1d04a2586842771a3
yhy940806 <yhy940806@163.com>
2023-03-05 18:21:09 +0800

    update env.dev

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: false,
-  apiUrl: 'https://staging-api.jeeve.com.au/staging/node/analytics',
-  homeUrl: 'https://staging-analytics.jeeve.com.au/',
-  solutionsUrl: 'https://staging-api.jeeve.com.au/staging/solutions',
+  apiUrl: 'https://test-api.jeeve.com.au/test/node/analytics',
+  homeUrl: 'https://test-analytics.jeeve.com.au/',
+  solutionsUrl: 'https://test-api.jeeve.com.au/test/solutions',
   stripeKey:
     'pk_test_51IwzWzDUubFKxLHp1i9fEqN0jFQTWPYOslUFJ2k84twcLejkiE4nnyq5HgsHqNlez8pGy9ZvKPR9AjO2vZ1th9JY00Kv8FlSSy'
 };
