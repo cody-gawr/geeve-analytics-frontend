bfa1b4ebadcd775e375659b36852dafcd2dd1f1c
zemin <likeredapple@outlook.com>
2023-08-31 14:55:28 +0800

    Add expenses show more button

--- src/newapp/dashboard/components/finances/none-trend/expenses/expenses.component.ts
+++ src/newapp/dashboard/components/finances/none-trend/expenses/expenses.component.ts
@@ -5,220 +5,220 @@ import { JeeveLineFillOptions } from "@/newapp/shared/utils";
 import { Component, OnInit, OnDestroy, Input } from "@angular/core";
 import { ChartOptions } from "chart.js";
 import _ from "lodash";
-import { Subject, takeUntil, combineLatest, map } from 'rxjs';
+import { Subject, takeUntil, combineLatest, map } from "rxjs";
+import { MkSelectExpensesModalComponent } from "../select-expenses-modal/select-expenses-modal.component";
+import { MatDialog } from "@angular/material/dialog";
 
 @Component({
-    selector: 'finance-expense-chart',
-    templateUrl: './expenses.component.html',
-    styleUrls: ['./expenses.component.scss']
+  selector: "finance-expense-chart",
+  templateUrl: "./expenses.component.html",
+  styleUrls: ["./expenses.component.scss"],
 })
 export class FinanceExpensesComponent implements OnInit, OnDestroy {
-    @Input() toolTip = '';
-    @Input() isFullMonths = false;
-    get isLoading$() {
-        return this.financeFacade.isLoadingFnExpenses$
-    }
+  @Input() toolTip = "";
+  @Input() isFullMonths = false;
+  get isLoading$() {
+    return this.financeFacade.isLoadingFnExpenses$;
+  }
 
-    destroy = new Subject<void>();
-    destroy$ = this.destroy.asObservable();
+  destroy = new Subject<void>();
+  destroy$ = this.destroy.asObservable();
 
-    selectedData: {value: number, name: string}[] = [];
-    unSelectedData: {value: number, name: string}[] = [];
-    isShowLabels = true;
-    isExplodeSlices = false;
-    arcWidth = 0.75;
-    isDoughnut = false;
-    isGradient = false;
-    isTooltipDisabled = false;
+  selectedData: { value: number; name: string }[] = [];
+  unSelectedData: { value: number; name: string }[] = [];
+  isShowLabels = true;
+  isExplodeSlices = false;
+  arcWidth = 0.75;
+  isDoughnut = false;
+  isGradient = false;
+  isTooltipDisabled = false;
 
-    datasets = [];
-    labels = [];
+  datasets = [];
+  labels = [];
 
-    get hasData$() {
-        return combineLatest([
-            this.clinicFacade.currentClinicId$
-        ])
-        .pipe(
-            takeUntil(this.destroy$),
-            map(([clinicId]) => {
-                if(typeof clinicId === 'string'){
-                    return this.datasets.length > 0;
-                }else{
-                    return this.selectedData.length > 0;
-                }
+  get hasData$() {
+    return combineLatest([this.clinicFacade.currentClinicId$]).pipe(
+      takeUntil(this.destroy$),
+      map(([clinicId]) => {
+        if (typeof clinicId === "string") {
+          return this.datasets.length > 0;
+        } else {
+          return this.selectedData.length > 0;
+        }
+      })
+    );
+  }
+
+  get isMultipleClinic$() {
+    return this.clinicFacade.currentClinicId$.pipe(
+      takeUntil(this.destroy$),
+      map((v) => typeof v == "string")
+    );
+  }
+
+  colorScheme = {
+    domain: [
+      "#6edbba",
+      "#abb3ff",
+      "#b0fffa",
+      "#ffb4b5",
+      "#d7f8ef",
+      "#fffdac",
+      "#fef0b8",
+      "#4ccfae",
+    ],
+  };
+
+  constructor(
+    private financeFacade: FinanceFacade,
+    private clinicFacade: ClinicFacade,
+    public dialog: MatDialog
+  ) {
+    combineLatest([
+      this.financeFacade.expensesData$,
+      this.financeFacade.expensesProduction$,
+      this.clinicFacade.currentClinicId$,
+    ])
+      .pipe(takeUntil(this.destroy$))
+      .subscribe(([expenses, production, clinicId]) => {
+        if (typeof clinicId === "string") {
+          this.datasets = [];
+          let i = 0;
+          _.chain(expenses)
+            .groupBy("accountName")
+            .map((items, accountName) => {
+              return {
+                items,
+                accountName,
+              };
             })
-        )
-    }
+            .value()
+            .forEach((v) => {
+              const bgColor = DoughnutChartColors[i];
+              i++;
+              this.datasets.push({
+                data: _.chain(v.items)
+                  .orderBy("clinicId", "asc")
+                  .value()
+                  .map(
+                    (item) =>
+                      _.round((item.expense / production) * 100 * 10) / 10
+                  ),
+                label: v.accountName,
+                backgroundColor: bgColor,
+                hoverBackgroundColor: bgColor,
+              });
+            });
+          this.labels = _.chain(expenses)
+            .unionBy((item) => item.clinicName)
+            .value()
+            .map((item) => item.clinicName);
+        } else {
+          if (production > 0) {
+            this.selectedData = [];
+            this.unSelectedData = [];
+            expenses.forEach((item, index) => {
+              const chartItem = {
+                name: `${item.accountName}--${item.expense}`,
+                value: _.round((item.expense / production) * 100 * 10) / 10,
+              };
 
-    get isMultipleClinic$(){
-        return this.clinicFacade.currentClinicId$.pipe(
-          takeUntil(this.destroy$),
-          map(v => typeof v == 'string')
-        )
-    }
+              if (index < 15) {
+                this.selectedData.push(chartItem);
+              } else {
+                this.unSelectedData.push(chartItem);
+              }
+            });
+          }
+        }
+      });
+  }
 
-    colorScheme = {
-        domain: [
-          '#6edbba',
-          '#abb3ff',
-          '#b0fffa',
-          '#ffb4b5',
-          '#d7f8ef',
-          '#fffdac',
-          '#fef0b8',
-          '#4ccfae'
-        ]
-    };
+  ngOnInit(): void {}
 
-    constructor(
-        private financeFacade: FinanceFacade,
-        private clinicFacade: ClinicFacade
-    ) {
-        combineLatest([
-            this.financeFacade.expensesData$,
-            this.financeFacade.expensesProduction$,
-            this.clinicFacade.currentClinicId$
-        ])
-        .pipe(
-            takeUntil(this.destroy$)
-        ).subscribe(([expenses, production, clinicId]) => {
+  ngOnDestroy(): void {
+    this.destroy.next();
+  }
 
-            if(typeof clinicId === 'string'){
-                this.datasets = [];
-                let i = 0;
-                _.chain(expenses).groupBy('accountName')
-                .map(
-                    (items, accountName) => {
-                        return {
-                            items,
-                            accountName
-                        }
-                    }
-                ).value().forEach(
-                    v => {
-                        const bgColor = DoughnutChartColors[i];
-                        i++;
-                        this.datasets.push({
-                            data: _.chain(v.items).orderBy('clinicId', 'asc')
-                            .value()
-                            .map(item => _.round((item.expense / production) * 100 * 10)/10),
-                            label: v.accountName,
-                            backgroundColor: bgColor,
-                            hoverBackgroundColor: bgColor
-                        });
-                    }
-                );
-                this.labels = _.chain(expenses).unionBy(item => item.clinicName)
-                .value()
-                .map(item => item.clinicName);
-                
-            }else{
-                if(production > 0){
-                    this.selectedData = [];
-                    this.unSelectedData = [];
-                    expenses.forEach((item, index) => {
-                        const chartItem = {
-                            name: `${item.accountName}--${item.expense}`,
-                            value: _.round((item.expense/production) * 100 * 10)/10
-                        };
-        
-                        if(index < 15){
-                            this.selectedData.push(chartItem);
-                        }else{
-                            this.unSelectedData.push(chartItem);
-                        }
-                    });
-                }
-            }
-
-        })
-    }
-
-    ngOnInit(): void {
-    }
-
-    ngOnDestroy(): void {
-        this.destroy.next();
-    }
-
-    pieTooltipText({ data, index }) {
-        const labl = data.name.split('--');
-        if(labl.length > 1){
-            const label = labl[0];
-            const exp = Math.round(labl[1])
-              .toString()
-              .replace(/\B(?=(\d{3})+(?!\d))/g, ',');
-            return `
+  pieTooltipText({ data, index }) {
+    const labl = data.name.split("--");
+    if (labl.length > 1) {
+      const label = labl[0];
+      const exp = Math.round(labl[1])
+        .toString()
+        .replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+      return `
               <span class="tooltip-label">${label}</span>
               <span class="tooltip-val"> ${data.value}% ($${exp})</span>
             `;
-        }else{
-            return '';
-        }
+    } else {
+      return "";
     }
+  }
 
-    pieLabelText(labels) {
-        return labels.split('--')[0];
-    }
+  pieLabelText(labels) {
+    return labels.split("--")[0];
+  }
 
-    public labelBarOptionsMultiPercentage: ChartOptions<'bar'> = {
-        elements: {
-          point: {
-            radius: 5,
-            hoverRadius: 7,
-            pointStyle: 'rectRounded',
-            hoverBorderWidth: 7
+  openExpensesDialog() {
+    const dialogRef = this.dialog.open(MkSelectExpensesModalComponent);
+    dialogRef.afterClosed().subscribe((result) => {
+      this.selectedData = result.selectedData;
+      this.unSelectedData = result.unSelectedData;
+    });
+  }
+
+  public labelBarOptionsMultiPercentage: ChartOptions<"bar"> = {
+    elements: {
+      point: {
+        radius: 5,
+        hoverRadius: 7,
+        pointStyle: "rectRounded",
+        hoverBorderWidth: 7,
+      },
+      line: JeeveLineFillOptions,
+    },
+    responsive: true,
+    maintainAspectRatio: false,
+    animation: {
+      duration: 500,
+      easing: "easeOutSine",
+    },
+    scales: {
+      x: {
+        stacked: true,
+        ticks: {
+          autoSkip: false,
+        },
+      },
+      y: {
+        stacked: true,
+        ticks: {
+          callback: (label: string | number) => {
+            return `${Number(label)}%`;
           },
-          line: JeeveLineFillOptions,
         },
-        responsive: true,
-        maintainAspectRatio: false,
-        animation: {
-          duration: 500,
-          easing: 'easeOutSine'
+      },
+    },
+    plugins: {
+      legend: {
+        display: false,
+      },
+      tooltip: {
+        displayColors: (ctx, options) => {
+          const tooltip = ctx.tooltip;
+          if (!tooltip) return true;
+          // disable displaying the color box;
+          return false;
         },
-        scales: {
-          x: 
-            {
-              stacked: true,
-              ticks: {
-                autoSkip: false
-              }
-            },
-          y: 
-            {
-              stacked: true,
-              ticks: {
-                callback: (label: string | number) => {
-                  return `${Number(label)}%`;
-                }
-              }
-            }
-        },
-        plugins: {
-          legend: {
-            display: false
+        mode: "x",
+        callbacks: {
+          label: (tooltipItem) => {
+            return `${tooltipItem.dataset.label}: ${tooltipItem.formattedValue}%`;
           },
-          tooltip: {
-            displayColors: (ctx, options) => {
-              const tooltip = ctx.tooltip;
-              if (!tooltip) return true;
-              // disable displaying the color box;
-              return false
-            },
-            mode: 'x',
-            callbacks: {
-              label: (
-                tooltipItem
-              ) => {
-                return `${tooltipItem.dataset.label}: ${
-                  tooltipItem.formattedValue
-                }%`;
-              },
-              title: () => ''
-            }
-          }
-        }
-    
-    };
+          title: () => "",
+        },
+      },
+    },
+  };
 }
---
+++ src/newapp/dashboard/components/finances/none-trend/select-expenses-modal/select-expenses-modal.component.ts
@@ -0,0 +1,199 @@
+import { ClinicFacade } from "@/newapp/clinic/facades/clinic.facade";
+import { DashboardFacade } from "@/newapp/dashboard/facades/dashboard.facade";
+import { FinanceFacade } from "@/newapp/dashboard/facades/finance.facade";
+import { MarketingFacade } from "@/newapp/dashboard/facades/marketing.facade";
+import { Component, Inject, OnInit, OnDestroy, Input } from "@angular/core";
+import {
+  MatDialog,
+  MAT_DIALOG_DATA,
+  MatDialogRef,
+} from "@angular/material/dialog";
+import _ from "lodash";
+import { Subject, takeUntil, combineLatest, map } from "rxjs";
+export interface DialogData {
+  title: string;
+}
+import Swal from "sweetalert2";
+
+@Component({
+  selector: "mk-select-expenses-modal",
+  templateUrl: "select-expenses-modal.component.html",
+  styleUrls: ["select-expenses-modal.component.scss"],
+})
+export class MkSelectExpensesModalComponent implements OnInit, OnDestroy {
+  destroy = new Subject<void>();
+  destroy$ = this.destroy.asObservable();
+
+  selectedData: { value: number; name: string }[] = [];
+  unSelectedData: { value: number; name: string }[] = [];
+
+  get isLoading$() {
+    return combineLatest([
+      this.financeFacade.isLoadingFnExpenses$,
+      //this.marketingFacade.isLoadingMkXeroAccounts$
+    ]).pipe(
+      takeUntil(this.destroy$),
+      map(([v1]) => v1)
+    );
+  }
+
+  // get isWaiting$(){
+  //     return combineLatest([
+  //         this.marketingFacade.isLoadingMkSaveMyobAccounts$,
+  //         this.marketingFacade.isLoadingMkSaveXeroAccounts$
+  //     ]).pipe(
+  //         takeUntil(this.destroy$),
+  //         map( ([v1, v2]) => v1 || v2)
+  //     )
+  // }
+
+  get connectedWith$() {
+    return this.dashboardFacade.connectedWith$.pipe(
+      takeUntil(this.destroy$),
+      map((v) => v)
+    );
+  }
+
+  constructor(
+    public dialogRef: MatDialogRef<MkSelectExpensesModalComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: DialogData,
+    private dashboardFacade: DashboardFacade,
+    private financeFacade: FinanceFacade,
+    private clinicFacade: ClinicFacade
+  ) {
+    // combineLatest([
+    //     this.dashboardFacade.connectedWith$,
+    //     this.marketingFacade.xeroAccounts$,
+    //     this.marketingFacade.myobAccounts$
+    // ]).pipe(
+    //     takeUntil(this.destroy$),
+    // ).subscribe(([isConnected, xeroAccounts, myobAccounts]) => {
+    //     if(isConnected == 'xero'){
+    //         this.selectedAccounts = xeroAccounts.data.selectedCategories;
+    //         const result = [];
+    //         for(let key in xeroAccounts.data.categories){
+    //             const val = xeroAccounts.data.categories[key];
+    //             if(!this.selectedAccounts.includes(val)){
+    //                 result.push(val);
+    //             }
+    //         }
+    //         this.unselectedAccounts = result;
+    //     }else if(isConnected == 'myob'){
+    //         this.selectedAccounts = myobAccounts.data.selectedCategories;
+    //         const result = [];
+    //         for(let key in myobAccounts.data.categories){
+    //             const val = myobAccounts.data.categories[key];
+    //             if(!this.selectedAccounts.includes(val)){
+    //                 result.push(val);
+    //             }
+    //         }
+    //         this.unselectedAccounts = result;
+    //     }else {
+    //         this.selectedAccounts = [];
+    //     }
+    // });
+
+    combineLatest([
+      this.financeFacade.expensesData$,
+      this.financeFacade.expensesProduction$,
+      this.clinicFacade.currentClinicId$,
+    ])
+      .pipe(takeUntil(this.destroy$))
+      .subscribe(([expenses, production, clinicId]) => {
+        //   if (typeof clinicId === "string") {
+        //     this.datasets = [];
+        //     let i = 0;
+        //     _.chain(expenses)
+        //       .groupBy("accountName")
+        //       .map((items, accountName) => {
+        //         return {
+        //           items,
+        //           accountName,
+        //         };
+        //       })
+        //       .value()
+        //       .forEach((v) => {
+        //         const bgColor = DoughnutChartColors[i];
+        //         i++;
+        //         this.datasets.push({
+        //           data: _.chain(v.items)
+        //             .orderBy("clinicId", "asc")
+        //             .value()
+        //             .map(
+        //               (item) =>
+        //                 _.round((item.expense / production) * 100 * 10) / 10
+        //             ),
+        //           label: v.accountName,
+        //           backgroundColor: bgColor,
+        //           hoverBackgroundColor: bgColor,
+        //         });
+        //       });
+        //     this.labels = _.chain(expenses)
+        //       .unionBy((item) => item.clinicName)
+        //       .value()
+        //       .map((item) => item.clinicName);
+        //   } else {
+        if (production > 0) {
+          this.selectedData = [];
+          this.unSelectedData = [];
+          expenses.forEach((item, index) => {
+            const chartItem = {
+              name: `${item.accountName}--${item.expense}`,
+              value: _.round((item.expense / production) * 100 * 10) / 10,
+            };
+
+            if (index < 15) {
+              this.selectedData.push(chartItem);
+            } else {
+              this.unSelectedData.push(chartItem);
+            }
+          });
+        }
+        //   }
+      });
+  }
+
+  ngOnInit(): void {}
+
+  ngOnDestroy(): void {
+    this.destroy.next();
+  }
+
+  onNoClick(): void {
+    this.dialogRef.close({
+      selectedData: this.selectedData,
+      unSelectedData: this.unSelectedData,
+    });
+  }
+
+  addToSelected(index: number) {
+    if (this.selectedData.length >= 20) {
+      Swal.fire({
+        icon: "error",
+        title: "Oops....",
+        text:
+          "You can select a maximum of " + 20 + " expense accounts at a time",
+      });
+    } else {
+      this.selectedData.push(this.unSelectedData[index]);
+      this.unSelectedData.splice(index, 1);
+    }
+  }
+
+  removeFromSelected(index: number) {
+    if (this.selectedData.length <= 20) {
+      Swal.fire({
+        icon: "error",
+        title: "Oops....",
+        text: "Minimum " + 20 + " expense account will remain selected",
+      });
+    } else {
+      this.unSelectedData.push(this.selectedData[index]);
+      this.selectedData.splice(index, 1);
+    }
+  }
+
+  refreshFinanceChart() {
+    this.onNoClick();
+  }
+}
