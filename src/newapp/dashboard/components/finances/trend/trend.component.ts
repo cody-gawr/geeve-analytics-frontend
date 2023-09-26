7a866b28f9793333fec1a9e341bcb5bf094756eb
zemin <likeredapple@outlook.com>
2023-09-26 13:34:39 +0800

    Fix chart tooltip in new finance page

--- src/newapp/dashboard/components/finances/trend/trend.component.ts
+++ src/newapp/dashboard/components/finances/trend/trend.component.ts
@@ -1,22 +1,43 @@
-import { Component, OnInit, OnDestroy } from "@angular/core";
-import { DashboardFacade } from "../../../facades/dashboard.facade";
-import { Subject, takeUntil, combineLatest, map } from "rxjs";
-import { FinanceFacade } from "../../../facades/finance.facade";
-import { LayoutFacade } from "@/newapp/layout/facades/layout.facade";
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { DashboardFacade } from '../../../facades/dashboard.facade';
+import { Subject, takeUntil, combineLatest, map } from 'rxjs';
+import { FinanceFacade } from '../../../facades/finance.facade';
+import { ClinicFacade } from '@/newapp/clinic/facades/clinic.facade';
 
 @Component({
-  selector: "trend-finances",
-  templateUrl: "./trend.component.html",
-  styleUrls: ["./trend.component.scss"],
+  selector: 'trend-finances',
+  templateUrl: './trend.component.html',
+  styleUrls: ['./trend.component.scss'],
 })
 export class TrendFinanceComponent implements OnInit, OnDestroy {
   destroy = new Subject<void>();
   destroy$ = this.destroy.asObservable();
 
+  get isMultipleClinic$() {
+    return this.clinicFacade.currentClinicId$.pipe(
+      takeUntil(this.destroy$),
+      map(v => typeof v == 'string')
+    );
+  }
+
+  get toolTipFnProductionPerVisit$() {
+    return combineLatest([
+      this.dashbordFacade.chartTips$,
+      this.isMultipleClinic$,
+    ]).pipe(
+      map(([c, v]) => {
+        if (c && c[v ? 95 : 30]) {
+          return c[v ? 95 : 30];
+        }
+        return null;
+      })
+    );
+  }
+
   constructor(
     private dashbordFacade: DashboardFacade,
     private financeFacade: FinanceFacade,
-    private layoutFacade: LayoutFacade
+    private clinicFacade: ClinicFacade
   ) {}
 
   ngOnInit(): void {}
@@ -34,17 +55,17 @@ export class TrendFinanceComponent implements OnInit, OnDestroy {
       map(([chartName, tips]) => {
         if (tips) {
           switch (chartName) {
-            case "Production":
+            case 'Production':
               return tips[31];
-            case "Collection":
+            case 'Collection':
               return tips[33];
-            case "Net Profit":
+            case 'Net Profit':
               return tips[26];
-            case "Net Profit %":
+            case 'Net Profit %':
               return tips[27];
           }
         }
-        return "";
+        return '';
       })
     );
   }
@@ -52,11 +73,11 @@ export class TrendFinanceComponent implements OnInit, OnDestroy {
   getChartTip(index: number) {
     return this.dashbordFacade.chartTips$.pipe(
       takeUntil(this.destroy$),
-      map((c) => {
+      map(c => {
         if (c && c[index]) {
           return c[index];
         }
-        return "";
+        return '';
       })
     );
   }
