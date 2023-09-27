17dfd816aaaed1d35e65e7b28e5cab971df51524
devmaxkyu <likeredapple@outlook.com>
2023-09-27 12:43:27 +0800

    wip: clinic-goals service

--- src/app/clinic-goals/clinic-goals.service.ts
+++ src/app/clinic-goals/clinic-goals.service.ts
@@ -1,71 +1,100 @@
-
-
-import {map} from 'rxjs/operators';
+import { map } from 'rxjs/operators';
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders, HttpResponse } from '@angular/common/http'
+import { HttpClient, HttpHeaders, HttpResponse } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { CookieService } from "ngx-cookie";
-import { environment } from "../../environments/environment";
-import { Router  } from '@angular/router';
+import { CookieService } from 'ngx-cookie';
+import { environment } from '../../environments/environment';
+import { Router } from '@angular/router';
 
 @Injectable()
 export class ClinicGoalsService {
+  public token: string;
+  private headers: HttpHeaders;
+  private apiUrl = environment.apiUrl;
+  public token_id;
 
-   public token: string;
-    private headers: HttpHeaders;
-    private apiUrl = environment.apiUrl;
-    public token_id;
+  constructor(
+    private http: HttpClient,
+    private _cookieService: CookieService,
+    private router: Router
+  ) {}
 
-    constructor(private http: HttpClient,private _cookieService: CookieService,private router: Router) {}
-
-    getHeaders(){
-        if(this._cookieService.get("user_type") != '1' && this._cookieService.get("user_type") != '2'){
-            this.token_id = this._cookieService.get("childid");
-        } else {
-            this.token_id= this._cookieService.get("userid");
-        }
-       let headers =  {headers: new HttpHeaders(), withCredentials: true, observe: 'response' as const };
-        return headers;
+  getHeaders() {
+    if (
+      this._cookieService.get('user_type') != '1' &&
+      this._cookieService.get('user_type') != '2'
+    ) {
+      this.token_id = this._cookieService.get('childid');
+    } else {
+      this.token_id = this._cookieService.get('userid');
     }
+    let headers = {
+      headers: new HttpHeaders(),
+      withCredentials: true,
+      observe: 'response' as const,
+    };
+    return headers;
+  }
 
-   // Get ClinicGoals
-    getClinicGoals(clinic_id=''): Observable<any> {
-        var header = this.getHeaders(); 
-        return this.http.get(this.apiUrl +"/Goals/goalGetClinic?clinic_id="+clinic_id, header)
-        .pipe(map((response: HttpResponse<Object>) => {
-                        return response;
-                    })
+  // Get ClinicGoals
+  getClinicGoals(clinic_id = ''): Observable<any> {
+    var header = this.getHeaders();
+    return this.http
+      .get(this.apiUrl + '/Goals/goalGetClinic?clinic_id=' + clinic_id, header)
+      .pipe(
+        map((response: HttpResponse<Object>) => {
+          return response;
+        })
+      );
+  }
+  // Get ClinicGoals updated
+  getGoalAllData(clinic_id = '', dentist_id, selectedYear): Observable<any> {
+    var header = this.getHeaders();
+    if (dentist_id == '') {
+      return this.http
+        .get(
+          this.apiUrl +
+            '/Goals/goalGetData?clinic_id=' +
+            clinic_id +
+            '&year=' +
+            selectedYear,
+          header
+        )
+        .pipe(
+          map((response: HttpResponse<Object>) => {
+            return response;
+          })
         );
     }
-    // Get ClinicGoals updated
-    getGoalAllData(clinic_id='',dentist_id,selectedYear): Observable<any> {
-        var header = this.getHeaders(); 
-        if(dentist_id == ''){
-            return this.http.get(this.apiUrl +"/Goals/goalGetData?clinic_id="+clinic_id+"&year="+selectedYear, header)
-                .pipe(map((response: HttpResponse<Object>) => {
-                        return response;
-                    })
-                );
-        }
-        return this.http.get(this.apiUrl +"/Goals/goalGetData?clinic_id="+clinic_id+"&dentist_id="+dentist_id+"&year="+selectedYear, header)
-            .pipe(map((response: HttpResponse<Object>) => {
-                return response;
-            })
-        );
-    }
-       // Get ClinicGoals
-    updateClinicGoals(clinicData, clinic_id=''): Observable<any> {
-            const formData = new FormData();
-        formData.append('goals', clinicData);
-        formData.append('clinic_id', clinic_id);
-        var header = this.getHeaders();
-        return this.http.post(this.apiUrl +"/Goals/goalAddClinic", formData,header)
-        .pipe(map((response: HttpResponse<Object>) => {
-                        return response;
-                    })
-        );
-    }
-
-       
+    return this.http
+      .get(
+        this.apiUrl +
+          '/Goals/goalGetData?clinic_id=' +
+          clinic_id +
+          '&dentist_id=' +
+          dentist_id +
+          '&year=' +
+          selectedYear,
+        header
+      )
+      .pipe(
+        map((response: HttpResponse<Object>) => {
+          return response;
+        })
+      );
+  }
+  // Get ClinicGoals
+  updateClinicGoals(clinicData, clinic_id = ''): Observable<any> {
+    const formData = new FormData();
+    formData.append('goals', clinicData);
+    formData.append('clinic_id', clinic_id);
+    var header = this.getHeaders();
+    return this.http
+      .post(this.apiUrl + '/Goals/goalAddClinic', formData, header)
+      .pipe(
+        map((response: HttpResponse<Object>) => {
+          return response;
+        })
+      );
+  }
 }
-
--- src/app/clinic-settings/goals/goals.component.ts
+++ src/app/clinic-settings/goals/goals.component.ts
@@ -1,7 +1,7 @@
 import { AfterViewInit, Component, Input, OnInit } from '@angular/core';
 import { UntypedFormBuilder, UntypedFormGroup } from '@angular/forms';
 import { Router } from '@angular/router';
-import { CookieService } from "ngx-cookie";
+import { CookieService } from 'ngx-cookie';
 import { ToastrService } from 'ngx-toastr';
 import { BehaviorSubject, combineLatest } from 'rxjs';
 import { takeUntil } from 'rxjs/operators';
@@ -14,9 +14,12 @@ import { ClinicSettingsService } from '../clinic-settings.service';
 @Component({
   selector: 'app-goals-settings',
   templateUrl: './goals.component.html',
-  styleUrls: ['./goals.component.scss']
+  styleUrls: ['./goals.component.scss'],
 })
-export class GoalsComponent extends BaseComponent implements OnInit, AfterViewInit {
+export class GoalsComponent
+  extends BaseComponent
+  implements OnInit, AfterViewInit
+{
   clinic_id$ = new BehaviorSubject<any>(null);
   clinicAnalysisForm: UntypedFormGroup;
   clinicProcedureForm: UntypedFormGroup;
@@ -26,18 +29,18 @@ export class GoalsComponent extends BaseComponent implements OnInit, AfterViewIn
   dentists: any = [];
   goalsData: any = {};
   clinicAnalysisGoals: any = [];
-  tabs: any = [];  
+  tabs: any = [];
   year = new Date().getFullYear();
   range: any = [];
   tabsOptions: string[] = [];
   selectedGoalCategory$ = new BehaviorSubject<any>('');
   selectedTab: any = 1;
-  Cconsultant:any ='';
-  selectedYear : any = this.year;
-  tabChanged :boolean = false;
+  Cconsultant: any = '';
+  selectedYear: any = this.year;
+  tabChanged: boolean = false;
   @Input() set clinicId(value: any) {
     this.clinic_id$.next(value);
-  };
+  }
 
   constructor(
     private clinicGoalsService: ClinicGoalsService,
@@ -53,52 +56,52 @@ export class GoalsComponent extends BaseComponent implements OnInit, AfterViewIn
     this.range.push(this.year - 1);
     this.range.push(this.year);
     for (var i = 1; i < 3; i++) {
-        this.range.push(this.year + i);
+      this.range.push(this.year + i);
     }
   }
 
   ngOnInit() {
     if (this.clinic_id$.value) this.getDentists(this.clinic_id$.value);
-
   }
 
   ngAfterViewInit() {
-    combineLatest([
-      this.clinic_id$,
-      this.selectedGoalCategory$
-    ])
-      .pipe(
-        takeUntil(this.destroyed$) 
-      ).subscribe(inputs => {
+    combineLatest([this.clinic_id$, this.selectedGoalCategory$])
+      .pipe(takeUntil(this.destroyed$))
+      .subscribe(inputs => {
         const [id, selectedGoalCategory] = inputs;
         if (id) {
-          this.getData(id,selectedGoalCategory,this.selectedYear);
+          this.getData(id, selectedGoalCategory, this.selectedYear);
         }
       });
   }
 
-
-  getData(id,selectedGoalCategory,selectedYear) 
-  {
-    this.clinicSettingsService.getClinicData.subscribe(res=>{
-      if (res.status == 200) {
-        this.Cconsultant = res.body.data[0]['consultant'];
-          this.clinicGoalsService.getGoalAllData(id,selectedGoalCategory,selectedYear).subscribe((res) => {
-            if (res.status == 200) {
-              this.getGoalsForTabsClinic(res.body.data);
-            } else if (res.status == 401) {
-              this.handleUnAuthorization();
-            }
-        }, error => {
-          console.log('error', error)
-        });
+  getData(id, selectedGoalCategory, selectedYear) {
+    this.clinicSettingsService.getClinicData.subscribe(
+      res => {
+        if (res.status == 200) {
+          this.Cconsultant = res.body.data[0]['consultant'];
+          this.clinicGoalsService
+            .getGoalAllData(id, selectedGoalCategory, selectedYear)
+            .subscribe(
+              res => {
+                if (res.status == 200) {
+                  this.getGoalsForTabsClinic(res.body.data);
+                } else if (res.status == 401) {
+                  this.handleUnAuthorization();
+                }
+              },
+              error => {
+                console.log('error', error);
+              }
+            );
+        } else if (res.status == 401) {
+          this.handleUnAuthorization();
+        }
+      },
+      error => {
+        console.log('error', error);
       }
-      else if (res.status == 401) {
-        this.handleUnAuthorization();
-      }
-    }, error => {
-      console.log('error', error)
-    });
+    );
     // this.dentistService.getClinicSettings(this.clinic_id$.value).subscribe((res) => {
     //   if (res.status == 200) {
     //     this.Cconsultant = res.body.data[0]['consultant'];
@@ -118,20 +121,21 @@ export class GoalsComponent extends BaseComponent implements OnInit, AfterViewIn
     // }, error => {
     //   console.log('error', error)
     // });
-    
   }
 
   getDentists(clinicID) {
-    this.dentistService.getDentists(clinicID).subscribe((res) => {
-      if (res.status == 200) {
-        this.dentists = res.body.data;
+    this.dentistService.getDentists(clinicID).subscribe(
+      res => {
+        if (res.status == 200) {
+          this.dentists = res.body.data;
+        } else if (res.status == 401) {
+          this.handleUnAuthorization();
+        }
+      },
+      error => {
+        console.log('error', error);
       }
-      else if (res.status == 401) {
-        this.handleUnAuthorization();
-      }
-    }, error => {
-      console.log('error', error)
-    });
+    );
   }
 
   // getClinicSettings(clinicID) {
@@ -155,104 +159,98 @@ export class GoalsComponent extends BaseComponent implements OnInit, AfterViewIn
 
   onTabYearChanged(event) {
     this.selectedYear = event;
-    combineLatest([
-      this.clinic_id$,
-      this.selectedGoalCategory$
-    ])
-      .pipe(
-        takeUntil(this.destroyed$) 
-      ).subscribe(inputs => {
+    combineLatest([this.clinic_id$, this.selectedGoalCategory$])
+      .pipe(takeUntil(this.destroyed$))
+      .subscribe(inputs => {
         const [id, selectedGoalCategory] = inputs;
         if (id) {
-          this.getData(id,selectedGoalCategory,this.selectedYear);
+          this.getData(id, selectedGoalCategory, this.selectedYear);
         }
       });
   }
 
-  handleGoalCategorySelection(event) {   
+  handleGoalCategorySelection(event) {
     this.selectedGoalCategory$.next(event);
   }
 
-
-getGoalsForTabsClinic(allGoals) {
-  this.tabs = [];
-  var temp = {
-    'd_id' : 0,
-    'd_name' : 0,
-    'charts' : [],
-  };
-  this.goalsData = {'year': this.selectedYear, 'goals' : {} };
-  allGoals.forEach((res)=> {
-    temp = {
-     'd_id' : res.id,
-     'd_name' : res.dashboard,
-     'charts' : [],
-   };
-   var chartTemp = {};
-   res.master_charts.forEach( (chart) => {
-      chartTemp = 
-      {
-        'c_id' : chart.id,
-        'c_dashboard_id' : chart.dashboard_id,
-        'c_name' : chart.chart,
-        'sign' : chart.sign,
-        'goals' : []
+  getGoalsForTabsClinic(allGoals) {
+    this.tabs = [];
+    var temp = {
+      d_id: 0,
+      d_name: 0,
+      charts: [],
+    };
+    this.goalsData = { year: this.selectedYear, goals: {} };
+    allGoals.forEach(res => {
+      temp = {
+        d_id: res.id,
+        d_name: res.dashboard,
+        charts: [],
       };
-      if( typeof(chart['all_dentist_goals']) != 'undefined' ){
-        chartTemp['goals'] = this.setGoalsPerMonth(chart['all_dentist_goals']);
-        this.goalsData['goals'][chart.id] = this.setGoalsPerMonth(chart['all_dentist_goals']);
-      } else {
-        chartTemp['goals'] = this.setGoalsPerMonth(chart['all_clinic_goals']);
-        this.goalsData['goals'][chart.id] = this.setGoalsPerMonth(chart['all_clinic_goals']);
-      }
-      
-      temp['charts'].push(chartTemp);
-      
-   });
-   this.tabs.push(temp);
-  });
-  var order = [63,64,1,49,15,66,65,8];
-  this.tabs[4].charts.sort(function(a, b) {
-    return order.indexOf(a.c_id) - order.indexOf(b.c_id);
-  })
-  this.tabs[4].d_name = "Prime KPI Report"
-  
-  // this.swap(this.tabs[4].charts,0,2);
+      var chartTemp = {};
+      res.master_charts.forEach(chart => {
+        chartTemp = {
+          c_id: chart.id,
+          c_dashboard_id: chart.dashboard_id,
+          c_name: chart.chart,
+          sign: chart.sign,
+          goals: [],
+        };
+        if (typeof chart['all_dentist_goals'] != 'undefined') {
+          chartTemp['goals'] = this.setGoalsPerMonth(
+            chart['all_dentist_goals']
+          );
+          this.goalsData['goals'][chart.id] = this.setGoalsPerMonth(
+            chart['all_dentist_goals']
+          );
+        } else {
+          chartTemp['goals'] = this.setGoalsPerMonth(chart['all_clinic_goals']);
+          this.goalsData['goals'][chart.id] = this.setGoalsPerMonth(
+            chart['all_clinic_goals']
+          );
+        }
 
-  // if(this.Cconsultant != 'prime'){
-  //   this.selectedTab = this.tabs[0].d_id;
-  // }else{
-  //   this.selectedTab = 10; // Kpi report
-  // }
-  if(this.Cconsultant == 'prime' && !this.tabChanged){
-    this.selectedTab = 10;
+        temp['charts'].push(chartTemp);
+      });
+      this.tabs.push(temp);
+    });
+    var order = [63, 64, 1, 49, 15, 66, 65, 8];
+    this.tabs[4].charts.sort(function (a, b) {
+      return order.indexOf(a.c_id) - order.indexOf(b.c_id);
+    });
+    this.tabs[4].d_name = 'Prime KPI Report';
+
+    // this.swap(this.tabs[4].charts,0,2);
+
+    // if(this.Cconsultant != 'prime'){
+    //   this.selectedTab = this.tabs[0].d_id;
+    // }else{
+    //   this.selectedTab = 10; // Kpi report
+    // }
+    if (this.Cconsultant == 'prime' && !this.tabChanged) {
+      this.selectedTab = 10;
+    }
   }
-}
 
-swap(arr, i, j){
-  var temp = arr[i];
-  arr[i] = arr[j];
-  arr[j] = temp;
-}
+  swap(arr, i, j) {
+    var temp = arr[i];
+    arr[i] = arr[j];
+    arr[j] = temp;
+  }
 
-setGoalsPerMonth(chartGoals)
-{  
-  var values= [-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1];     
-    chartGoals.forEach( (chardValues) => {
-      for(var i=0; i <= 11; i++ ){
-        var d1 = new Date(this.selectedYear,i,1);
+  setGoalsPerMonth(chartGoals) {
+    var values = [-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1];
+    chartGoals.forEach(chardValues => {
+      for (var i = 0; i <= 11; i++) {
+        var d1 = new Date(this.selectedYear, i, 1);
         var d2 = new Date(chardValues.start_date);
-        if(d1.getMonth() === d2.getMonth())
-        {
+        if (d1.getMonth() === d2.getMonth()) {
           values[i] = chardValues.value;
         }
-        
       }
     });
     return values;
-}
-
-
+  }
 
   getGoalsForTabs(allGoals) {
     let start = 0;
@@ -264,7 +262,7 @@ setGoalsPerMonth(chartGoals)
       for (let i = start; i <= end; i++) {
         this.tabs[key].patchValues = {
           ...this.tabs[key].patchValues,
-          [this.tabs[key].controls[index].name]: allGoals[i].value
+          [this.tabs[key].controls[index].name]: allGoals[i].value,
         };
         index++;
       }
@@ -272,10 +270,7 @@ setGoalsPerMonth(chartGoals)
     });
   }
 
-
-
   onSubmit() {
-
     let myJsonString = JSON.stringify(this.goalsData);
     $('.ajax-loader').show();
     if (this.selectedGoalCategory$.value === '') {
@@ -287,80 +282,110 @@ setGoalsPerMonth(chartGoals)
   }
 
   updateClinicGoals(formData) {
-    this.clinicGoalsService.updateClinicGoals(formData, this.clinic_id$.value).subscribe((res) => {
-      $('.ajax-loader').hide();
-      if (res.status == 200) {
-        this.toastr.success('Clinic Goals Updated');
-        this.getData(this.clinic_id$.value,this.selectedGoalCategory$.value,this.selectedYear);
-      }
-      else if (res.status == 401) {
-        this.handleUnAuthorization();
-      }
-    }, error => {
-      console.log('error', error);
-      $('.ajax-loader').hide();
-    });
+    this.clinicGoalsService
+      .updateClinicGoals(formData, this.clinic_id$.value)
+      .subscribe(
+        res => {
+          $('.ajax-loader').hide();
+          if (res.status == 200) {
+            this.toastr.success('Clinic Goals Updated');
+            this.getData(
+              this.clinic_id$.value,
+              this.selectedGoalCategory$.value,
+              this.selectedYear
+            );
+          } else if (res.status == 401) {
+            this.handleUnAuthorization();
+          }
+        },
+        error => {
+          console.log('error', error);
+          $('.ajax-loader').hide();
+        }
+      );
   }
 
   updateDentistGoals(formData) {
-    this.dentistGoalsService.updateDentistGoals(formData, this.clinic_id$.value, this.selectedGoalCategory$.value).subscribe((res) => {
-      $('.ajax-loader').hide();
-      if (res.status == 200) {
-        this.toastr.success('Dentist Goals Updated');
-        this.getData(this.clinic_id$.value,this.selectedGoalCategory$.value,this.selectedYear);
-      } else if (res.status == 401) {
-        this.handleUnAuthorization();
-      }
-    }, error => {
-      console.log('error', error);
-      $('.ajax-loader').hide();
-    });
+    this.dentistGoalsService
+      .updateDentistGoals(
+        formData,
+        this.clinic_id$.value,
+        this.selectedGoalCategory$.value
+      )
+      .subscribe(
+        res => {
+          $('.ajax-loader').hide();
+          if (res.status == 200) {
+            this.toastr.success('Dentist Goals Updated');
+            this.getData(
+              this.clinic_id$.value,
+              this.selectedGoalCategory$.value,
+              this.selectedYear
+            );
+          } else if (res.status == 401) {
+            this.handleUnAuthorization();
+          }
+        },
+        error => {
+          console.log('error', error);
+          $('.ajax-loader').hide();
+        }
+      );
   }
 
   handleUnAuthorization() {
-    this._cookieService.put("username", '');
-    this._cookieService.put("email", '');
-    this._cookieService.put("userid", '');
+    this._cookieService.put('username', '');
+    this._cookieService.put('email', '');
+    this._cookieService.put('userid', '');
     this.router.navigateByUrl('/login');
   }
-  updateName(name){
+  updateName(name) {
     var name = name.split('-');
-    if(name[1]){
+    if (name[1]) {
       return name[1];
     }
     return name;
   }
 
-  onKeyUp(id,val,event,sn, tab){
-    if($(event.target).hasClass('sign%') && val > 100){
+  onKeyUp(id, val, event, sn, tab) {
+    if ($(event.target).hasClass('sign%') && val > 100) {
       val = 100;
-    }  else if(val == ''){
+    } else if (val == '') {
       val = '';
-    } 
+    }
     $(event.target).val(val);
     val = +val;
     this.goalsData['goals'][id][sn] = val == 0 ? -1 : val;
-    if(tab === "Prime KPI Report"){
-      let product = +$( "input[name='goal63"+sn+"']" ).val() * +$( "input[name='goal64"+sn+"']" ).val();
-      $( "input[name='goal1"+sn+"']" ).val(product == 0 ? "" : product);
+    if (tab === 'Prime KPI Report') {
+      let product =
+        +$("input[name='goal63" + sn + "']").val() *
+        +$("input[name='goal64" + sn + "']").val();
+      $("input[name='goal1" + sn + "']").val(product == 0 ? '' : product);
       this.goalsData['goals'][1][sn] = product == 0 ? -1 : product;
     }
   }
 
-//   onBlur(id,val,event,sn){    
-//     event.preventDefault();
-//     event.stopPropagation();
-//     if($(event.target).hasClass('sign%') && val > 100){
-//       val = 100;
-//     }  else if(val == ''){
-//       val = '';
-//     } 
-//    $(event.target).val(val);
-//    this.goalsData['goals'][parseInt(id)][parseInt(sn)] = parseInt(val);
-//  }
+  //   onBlur(id,val,event,sn){
+  //     event.preventDefault();
+  //     event.stopPropagation();
+  //     if($(event.target).hasClass('sign%') && val > 100){
+  //       val = 100;
+  //     }  else if(val == ''){
+  //       val = '';
+  //     }
+  //    $(event.target).val(val);
+  //    this.goalsData['goals'][parseInt(id)][parseInt(sn)] = parseInt(val);
+  //  }
 
- keyDown(event){
-    if((event.keyCode < 37 || event.keyCode > 40) && (event.keyCode < 48 || event.keyCode > 57) && (event.keyCode < 96 || event.keyCode > 105) && event.keyCode != 9 && event.keyCode != 8 && event.keyCode != 46){
+  keyDown(event) {
+    if (
+      (event.keyCode < 37 || event.keyCode > 40) &&
+      (event.keyCode < 48 || event.keyCode > 57) &&
+      (event.keyCode < 96 || event.keyCode > 105) &&
+      event.keyCode != 9 &&
+      event.keyCode != 8 &&
+      event.keyCode != 46
+    ) {
       event.preventDefault();
     }
   }
