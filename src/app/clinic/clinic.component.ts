ee05b0a81c42edb717fc9d7691d0dc256febb7ac
zemin <likeredapple@outlook.com>
2024-04-09 15:06:35 +0800

    Fix bug in clinics settings page

--- src/app/clinic/clinic.component.ts
+++ src/app/clinic/clinic.component.ts
@@ -320,8 +320,8 @@ export class ClinicComponent implements AfterViewInit {
           this.rows = res.body.data;
           if (res.body.data.length > 0) {
             this.temp = [...res.body.data];
-            this.clinicscount = res.body.data[0]['config_user'].clinics_count;
-            this.createdClinicsCount = res.body.total;
+            //this.clinicscount = res.body.data[0]['config_user'].clinics_count;
+            this.createdClinicsCount = res.body?.data.length ?? 0;
             this.table = data;
           }
         } else if (res.status == 401) {
