d1464937d0e24c3f70ec1bb34e3d310490077944
GT <gary@jeeve.com.au>
2025-05-07 08:38:33 +1000

    fix youtube video

--- src/app/shared/youtube-preview/youtube-preview.component.ts
+++ src/app/shared/youtube-preview/youtube-preview.component.ts
@@ -8,29 +8,11 @@ import { Component, HostListener, Input, OnInit } from '@angular/core';
 export class YoutubePreviewComponent implements OnInit {
   @Input() videoId!: string;
 
-  public isHovered: boolean = false;
-
-  public isPlaying: boolean = false;
-
-  get thumbnailUrl(): string {
-    return `https://img.youtube.com/vi/${this.videoId}/hqdefault.jpg`;
-  }
-
   get videoUrl(): string {
-    return `https://www.youtube.com/embed/${this.videoId}?autoplay=1&mute=1&controls=0&showinfo=0&rel=0&modestbranding=1`;
+    return `https://www.youtube.com/embed/${this.videoId}?rel=0&modestbranding=1`;
   }
 
   constructor() {}
 
   ngOnInit() {}
-
-  @HostListener('mouseenter')
-  onMouseEnter() {
-    this.isHovered = true;
-  }
-
-  @HostListener('mouseleave')
-  onMouseLeave() {
-    this.isHovered = false;
-  }
 }
